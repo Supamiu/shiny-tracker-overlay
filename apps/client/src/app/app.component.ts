3339429f3351a0af06179cb9c229734161156374
Flavien Normand <contact@flavien-normand.fr>
2021-12-04 14:29:41 +0100

    initial commit

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shiny-hunting-overlay-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'client';
+}
